db2460bfa1aabf9e1e8ee2359fc738233d9e5404
Alessandro Malhotra <alessandro244@outlook.com>
2021-09-17 18:50:42 +0100

    3 Changes
    1. text for the second text field has been put in.
    
    2. currency delegate file been created for second text field
    
    3. changed implementation in zipcode so takes no more than 5 characters

---
+++ TextFieldApp/TextFieldApp/TextField Delegates/CurrencyDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  CurrencyDelegate.swift
+//  TextFieldApp
+//
+//  Created by Alessandro Malhotra on 17/09/2021.
+//
+
+import Foundation
+
+import UIKit
+
+class CurrencyDelegte: NSObject, UITextFieldDelegate {
+    
+    ///  dollar sign field that begins with the text $0.00, and then fills in the dollar figure as digits are added. For example, typing 4-2-7-5 would produce $0.00, $0.04, $0.42, $4.27, $42.75.
+    /// Update the text in textfield to increment as user types the digits, implent func text field method to update the text
+    
+    /// When writing the CashTextFieldDelegate a good first step is to create an Int that represents the number of pennies in the value entered by the user. Once you have that Int, it is easier to construct the correctly formatted cash string.
+    /// The Foundation class, NumberFormatter, may come in handy when formatting your cash string. Or you can write your own custom function.
+    
+}
--- TextFieldApp/TextFieldApp/TextField Delegates/ZipCodeDelegate.swift
+++ TextFieldApp/TextFieldApp/TextField Delegates/ZipCodeDelegate.swift
@@ -11,14 +11,12 @@ import UIKit
 
 class ZipCodeDelegate: NSObject, UITextFieldDelegate {
     
-    var count = 0
-    
-    
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         
-        let newText = textField.text! as NSString
+        var newText = textField.text! as NSString
+        newText = newText.replacingCharacters(in: range, with: string) as NSString
         
-        return newText.length < 5
+        return newText.length <= 5
     }
     
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
