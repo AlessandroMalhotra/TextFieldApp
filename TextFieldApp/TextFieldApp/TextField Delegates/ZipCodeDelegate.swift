679e946941cc5e54312eb3e3c5b9b1c567a73221
Alessandro Malhotra <alessandro244@outlook.com>
2021-09-17 11:01:44 +0100

    Various Implementations
    1. Created text field app, created text fields and labels in view.
    
    2. initialsied those labels and text fields in vc
    
    3. Created new folder and delegate file for text field1
    
    4. Initialised the delegate file and set the text field one delegate to be zip code

---
+++ TextFieldApp/TextFieldApp/TextField Delegates/ZipCodeDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  ZipCodeDelegate.swift
+//  TextFieldApp
+//
+//  Created by Alessandro Malhotra on 17/09/2021.
+//
+
+import Foundation
+
+import UIKit
+
+class ZipCodeDelegate: NSObject, UITextFieldDelegate {
+    
+    /// The text field can only allow 5 digits, once you get 5 digits can't type anymore
+    /// keyboard needs to be a number pad, can do this in storyboard under attributes inspector
+    /// revist emoji delegate for line 40/41 may need the code there 
+}
---
+++ TextFieldApp/TextFieldApp/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  TextFieldApp
+//
+//  Created by Alessandro Malhotra on 17/09/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    // MARK: Outlets
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var countLabel: UILabel!
+    
+    // MARK: Delegate Objects
+    
+    let zipCodeDelegate = ZipCodeDelegate()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        // Set the three delegates
+        self.textField1.delegate = zipCodeDelegate
+    }
+
+
+}
+
